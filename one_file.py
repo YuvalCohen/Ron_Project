d8b741a2c902d49c809d838954fe4c2c27db5e83
YuvalCohen <yuval.cohen@etradingsoftware.com>
2018-12-17 15:58:24 +0000

    Add files via upload

--- one_file.py
+++ one_file.py
@@ -4,46 +4,63 @@ import getpass
 from pathlib import Path
 import pickle
 
+def main_game():
+    game_settings = GameSettings()
+    game_users = GameUsers()
 
-game_settings = GameSettings()
-game_users = GameUsers()
+    introduction() ## print introduction
 
-introduction() ## print introduction
-while game_users.number_of_users() < game_settings.num_players :
-    game_users.add_user()
+    while True:
+        ## If there are not yet enough users, then there is a need to first add user(s):
+        ## Note that changing the settings [num_players] may require more than 2 users!
+        while game_users.number_of_users() < game_settings.num_players :
+            game_users.add_user()
+        
+        ## prompt the main menue:
+        prompt = ("Enter 1 to add a user\n"
+                "Enter 2 to play the game\n"
+                "Enter 3 to view user records\n"
+                "or    0 to to exit (terminate) the game\n"
+                ">>> ")
+                
+        res = select_number(prompt, 0, 9, None) ## user selection
+        if res == 1: # add user
+            game_users.add_user()
+        elif res == 2: # run game
+            players = []
+            for i in range(game_settings.num_players):
+                players += [ game_users.select_user(players) ]
+                if players[i] == None :
+                    break            
+            
+            play_again = True
+            while play_again: ## play many games with the same players and settings
+                
+                game = Game(players, game_settings) ## set a new game
+                winner_loser = game.run()  ## run the game
+                
+                if winner_loser != None:  ## if there is a winner and a loser, we store them in game users:
+                    game_users.user_win(winner_loser[0]) ## store the winner
+                    game_users.user_lost(winner_loser[1]) ## store the loser
+                play_again = query_yes_no("Would you like to play again?") ## play again?
+            
+        elif res == 3: # print the user recors
+            game_users.user_records()
+        
+        elif res == 9: # advance change settings
+            game_settings.change_settings()
+        elif res == 0: #terminate, exit!
+            break
 
-while True:
-	res = select({
-		1: "Add a user",
-		2: "Play a game",
-		3: "Print player records"
-		})
-    res = main_selection()
-    if res = 1: # add user
-        game_users.add_user()
-    elif res = 2: # run game
-        for i in range(game_settings.num_players):
-            players[i] = game_users.select_user()
-            if players[i] == None :
-                break
-		
-		game = Game(players, game_settings) ## these players are going to play now
-		play_again = True
-		while play_again: ## play many games with the same players and settings
-			winner_loser = game.run()  ## run the game
-			if winner_loser != None:  ## if there is a winner and a loser, we store them in game users:
-				game_users.win(winner_loser[0]) ## store the winner
-				game_users.lost(winner_loser[1]) ## store the loser
-			play_again = query_yes_no("Would you like to play again?") ## play again?
-		
-	elif res = 3: # run game
-		game_users.user_records()
-	
-	elif res = 9: # advance change settings
-	
-	elif res = 0: # advance change settings
-		break
-
+def introduction():
+    print ("""
+===================================================
+= Introduction") =
+= Introduction: blah blah blah... Ron, please complete the introduction with good English")
+= "blah blah blah...
+===================================================
+""")
+            
 ### Game settings ### 
 """The advance game settings 
 It store most of the constant of the games. 
@@ -52,9 +69,9 @@ changing the settings helped me to test different feature and code
 
 class GameSettings:
     def __init__(self):
-		self.initialize_defaults()
-		
-	def initialize_defaults(self)
+        self.initialize_defaults()
+        
+    def initialize_defaults(self):
         self.cube_size = 6
         self.num_turns = 5
         self.bonus = [10, -5] # 10 points bonus for even rolls, -5 penalty for odd rolls
@@ -63,42 +80,69 @@ class GameSettings:
         self.double_num_of_dice = 1 # the number of dice to roll when double
         self.draw_num_of_dice = 1 # the number of dice to roll after the main game ended at draw
 
-	def change_settings(self):
-		while True:
-			print ("Enter 1 to change cube size, default: 6")
-			print ("Enter 2 to change number of turns, default: 5")
-			print ("Enter 3 to change even number bonus (or penalty), default: 10")
-			print ("Enter 4 to change odd number bonus (or penalty), default: -5")
-			print ("Enter 5 to change the number of players in a game, default: 2")
-			print ("Enter 6 to change the number of dice in each regular roll, default: 2"
-			print ("Enter 7 to change the number of dice to roll when a double occur, default: 1")
-			print ("Enter 8 to change the number of dice to roll after the main game ended at draw, default: 1")
-			print ("Enter 9 to set all settings back to defaults")
+    def change_settings(self):
+        while True:
+            prompt = ("Enter 1 to change cube size \n"
+                "Enter 2 to change number of turns \n"
+                "Enter 3 to change even number bonus (or penalty) \n"
+                "Enter 4 to change odd number bonus (or penalty)  \n"
+                "Enter 5 to change the number of players in a game  \n"
+                "Enter 6 to change the number of dice in each regular roll \n"
+                "Enter 7 to change the number of dice to roll when a double occur  \n"
+                "Enter 8 to change the number of dice to roll after the main game ended at draw \n"
+                "Enter 9 to set all settings back to defaults \n"
+                "or    0 to exit to the main menu \n"
+                ">>> ")
 
-			selection = select_number("Enter 0 to exit to the main menu", 0, 9, None)
-			if selection == 1:
-				self.cube_size = select_number("Please enter the cube size", 2, None, 6)
-			elif selection == 2:
-				self.num_turns = select_number("Please enter the number of turns", 0, None, 5)
-			elif selection == 3:
-				self.bonus[0] = select_number("Please enter even number bonus (negative for penalty)", None, None, 10)
-			elif selection == 4:
-				self.bonus[1] = select_number("Please enter odd number bonus (negative for penalty)", None, None, -5)
-			elif selection == 5:
-				print("When the number of players is different than 2, there is no draw (even if all players ended with the same score)"
-				print("and the winner and loser are not recorded")
-				self.num_players = select_number("Please enter the number of players in a game", 2, None, 2)
-			elif selection == 6:
-				self.num_of_dice = select_number("Please enter the number of dice in each regular roll", 0, None, 2)
-			elif selection == 7:
-				self.double_num_of_dice = select_number("Please enter the number of dice to roll when a double occur", 0, None, 1)
-			elif selection == 8:
-				self.draw_num_of_dice = select_number("Please enter the number of dice to roll after the main game ended at draw", 1, None, 1)
-			elif selection == :
-				self.initialize_defaults()
-			else :
-				break
-				
+            selection = select_number(prompt, 0, 9, None)
+            if selection == 1:
+                prompt = ("Please enter the cube size \n"
+                    "The default (recommended) value is 6 and the current value is " + str(self.cube_size) + "\n"
+                    ">>> ")
+                self.cube_size = select_number(prompt, 2, None, 6)
+            elif selection == 2:
+                prompt = ("Please enter the number of turns \n"
+                    "The default (recommended) value is 5 and the current value is " + str(self.num_turns) + "\n"
+                    ">>> ")
+                self.num_turns = select_number(prompt, 0, None, 5)
+            elif selection == 3:
+                prompt = ("Please enter even number bonus (negative for penalty) \n"
+                    "The default (recommended) value is 10 and the current value is " + str(self.bonus[0]) + "\n"
+                    ">>> ")
+                self.bonus[0] = select_number(prompt, None, None, 10)
+            elif selection == 4:
+                prompt = ("Please enter odd number bonus (negative for penalty) \n"
+                    "The default (recommended) value is -5 and the current value is " + str(self.bonus[1]) + "\n"
+                    ">>> ")
+                self.bonus[1] = select_number(prompt, None, None, -5)
+            elif selection == 5:
+                print("IMPORTANT NOTE: when the number of players is different than 2:")
+                print("=============== there is no draw (even if all players ended with the same score)")
+                print("=============== and the winner and loser are not recorded")
+                prompt = ("Please enter the number of players in a game \n"
+                    "The default (recommended) value is 2 and the current value is " + str(self.num_players) + "\n"
+                    ">>> ")
+                self.num_players = select_number(prompt, 2, None, 2)
+            elif selection == 6:
+                prompt = ("Please enter the number of dice in each regular roll \n"
+                    "The default (recommended) value is 2 and the current value is " + str(self.num_of_dice) + "\n"
+                    ">>> ")
+                self.num_of_dice = select_number(prompt, 0, None, 2)
+            elif selection == 7:
+                prompt = ("Please enter the number of dice to roll when a double occur \n"
+                    "The default (recommended) value is 1 and the current value is " + str(self.double_num_of_dice) + "\n"
+                    ">>> ")
+                self.double_num_of_dice = select_number(prompt, 0, None, 1)
+            elif selection == 8:
+                prompt = ("Please enter the number of dice to roll after the main game ended at draw \n"
+                    "The default (recommended) value is 1 and the current value is " + str(self.draw_num_of_dice) + "\n"
+                    ">>> ")
+                self.draw_num_of_dice = select_number("Please enter the number of dice to roll after the main game ended at draw  >>> ", 1, None, 1)
+            elif selection == 9:
+                self.initialize_defaults()
+            else :
+                break
+                
 
 class Game:
     def __init__(self, users, settings):
@@ -110,11 +154,11 @@ class Game:
 
     def run(self):
         for i in range(self.s.num_turns) :
-            print("=== Turn #", i+1, "===")
+            print("===== Turn #", i+1, "=====")
             for j in range(self.s.num_players):
-                print("Player: ", self.users[j])
+                print("*** Player: ", self.users[j], "***")
                 points = self.play(j, self.s.num_of_dice, True)
-				
+                
             print ("The score at the end of turn #", i+1)
             for j in range(self.s.num_players):
                 print(self.users[j], "has", self.scores[j], "points")
@@ -122,9 +166,10 @@ class Game:
         if self.s.num_players == 2:
             i = 0
             while self.scores[0] == self.scores[1]:
-                print("Draw!", self.scores[0],  " extra turn #", i+1, " roll single dice")
+                i += 1
+                print("Draw!", self.scores[0],  " extra turn #", i, " roll single dice")
                 for j in range(self.s.num_players):
-                    print("Player:", self.users[j])
+                    print("*** Player:", self.users[j], "***")
                     self.play(j, self.s.draw_num_of_dice, False)
 
             print ("The scores:")
@@ -133,23 +178,24 @@ class Game:
                 winner = 1
             print("The winner is", self.users[winner], "with", self.scores[winner], "points")
             print(self.users[1-winner], "receive", self.scores[1-winner], "points")
+            return [ self.users[winner], self.users[1-winner] ]
 
 
     def bonus(self, roll, player_number):
-        if roll.len() == 2 and roll[0] == roll[1] : ### Double!! roll again:
-            print("*** You rolled double and you have a bonus roll ***: ", roll[0])
-			play(player_number, self.s.double_num_of_dice, False)
-            return 0
+        if len(roll) == 2 and roll[0] == roll[1] : ### Double!! roll again:
+            print("~ ", self.users[player_number], "You rolled double", roll[0], "and you have a bonus roll ~")
+            return self.play(player_number, self.s.double_num_of_dice, False) ## roll the dice again! (note this is a recusive call!!!)
+
         else: # Bonus
             odd = (roll[0] + roll[1]) % 2 ## 0 for even, 1 for odd
             bonus = self.s.bonus[ odd ]
             if bonus > 0:
-                print("Lucky roll! you rolled", roll, "and received", bonus, " bonus points")
+                print("Lucky roll! you received", bonus, " bonus points")
             elif bonus < 0:
-                print("You rolled", roll, " unlucky, you lose", -bonus, " points")
-            else: ## no bonus, i.e. bunus must 0
+                print("Unlucky, you lose", -bonus, " points")
+            ## else: ## no bonus, i.e. bunus must 0
                 
-			self.scores[player_number] += bonus
+            self.scores[player_number] += bonus ## add the bonus to the score
             return bonus
 
     def play(self, player_number, num_of_dice, add_bonus): # i is the number of turn, j is the number of user that plays
@@ -160,17 +206,17 @@ class Game:
             r = random.randint(1, self.s.cube_size)
             roll_score += r
             roll +=  [ r ]
-		
-		print("You rolled:", roll, "and you scored", roll_score, "points")
-		self.scores[player_number] += roll_score
-		
+        
+        print("You rolled:", roll, "and you scored", roll_score, "points")
+        self.scores[player_number] += roll_score
+        
         if add_bonus:
             roll_score += self.bonus(roll, player_number)
             print(self.users[player_number], "your score for this turn is", roll_score, "points")
             
         if self.scores[player_number] < 0:
             self.scores[player_number] = 0
-		return roll_score
+        return roll_score
 
 
 def test_game():
@@ -240,7 +286,7 @@ class GameUsers:
         self.store()
         return username
     
-    def select_user(self):
+    def select_user(self, already_selected):
         print ("Selecting a user:")
         username = None
         while True :
@@ -249,6 +295,8 @@ class GameUsers:
                 return None
             elif username not in self.s :
                 print("Sorry, user name " + username + " does not exists" )
+			elif username in already_selected:
+				print("User " + username + " already selected, please select a different user" )
             else:
                 break
 
@@ -262,17 +310,17 @@ class GameUsers:
         
     
     def user_win(self, username):
-        self.s[username]['win'] += 1
-        store()
+        self.s[username]['win'] += 1  ## increment the number of wins
+        self.store() ## Store in pickle file
     def user_lost(self, username):
-        self.s[username]['lost'] += 1
-        store()
-    def user_records(self):
-        for username, statistic in sorted(self.s):
-            print (username, "win", statistic['win'], "lost", statistic['lost'])
+        self.s[username]['lost'] += 1 ## increment the number of losts
+        self.store() ## Store in pickle file
+    def user_records(self): ## Print the records (except the passwords!)
+        for username in sorted(self.s.keys()): # iterate sorted, nice.
+            print (username, "won", self.s[username]['win'], "games and lost", self.s[username]['lost'], "games")
         
     def number_of_users(self):
-        return self.s.len()
+        return len(self.s)
 
 
 
@@ -308,29 +356,29 @@ def query_yes_no(question, default="no"):
             return valid[choice]
         else:
             print("Please respond with 'yes' or 'no' (or 'y' or 'n').\n")
-			
+            
 def encrypt (s): ## I don't know how to encrypt or decrypt
     return s
 def decrypt (s): ## I don't know how to encrypt or decrypt
     return s
 
 def select_number(prompt, minimum, maximum, default):
-	if default is not None
-		prompt += " [default: " + str(default) + "]"
-	whilt True:
-		i = input(promprt)
-		if i == "" and default is not None:
-			return default
-		try:
-			res = int(i)
-		except ValueError:
-			continue
-		if minimum is not None and res < minimum :
-			print ("The number must be >=", minimum)
-		elif maximum is not None and res > maximum :
-			print ("The number must be <=", maximum)
-		else:
-			return res
-	
-		
-		
+    if default is not None :
+        prompt += " [default: " + str(default) + "]"
+    while True:
+        i = input(prompt)
+        if i == "" and default is not None:
+            return default
+        try:
+            res = int(i)
+        except ValueError:
+            continue
+        if minimum is not None and res < minimum :
+            print ("The number must be >=", minimum)
+        elif maximum is not None and res > maximum :
+            print ("The number must be <=", maximum)
+        else:
+            return res
+    
+        
+main_game()
